3a85c644d0e47c4939b9890468e6154f1c68b395
Felipe Rocha <feliperocha.dev27@gmail.com>
2025-03-19 17:22:21 -0300

    first commit, componente de tabela criado

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Tabela } from './components/Tabela'
+
+export function App() {
+  return (
+    <div className="flex flex-1 flex-col bg-gray-950 h-screen items-center justify-center text-white">
+      <h1 className="text-5xl mb-5">Pedidos</h1>
+      <Tabela />
+    </div>
+  )
+}
