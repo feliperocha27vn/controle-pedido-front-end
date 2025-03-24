47376bf9415e119e44af2e58a74d6b3f736bcce0
Felipe Rocha <feliperocha.dev27@gmail.com>
2025-03-24 10:33:36 -0300

    instalando ShadcnUI

--- src/components/Tabela.tsx
+++ src/components/Tabela.tsx
@@ -1,49 +1,7 @@
-// Bibliotecas
-import { AgGridReact } from 'ag-grid-react'
-import {
-  AllCommunityModule,
-  colorSchemeDarkBlue,
-  ModuleRegistry,
-} from 'ag-grid-community'
-import { themeQuartz } from 'ag-grid-community'
-
-// Estados
-import { useState } from 'react'
-
-// Tipagem
-import type { ColDef } from 'ag-grid-community'
-
-ModuleRegistry.registerModules([AllCommunityModule])
-
-export async function Tabela() {
-  const [rowData, setRowData] = useState([
-    { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
-    { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
-    { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
-  ])
-
-  const colDefs: ColDef[] = [
-    { field: 'cliente', headerName: 'Cliente' },
-    { field: 'valor', headerName: 'Valor' },
-    { field: 'situcao', headerName: 'Situação' },
-    { field: 'id', headerName: 'Ver Detalhes' },
-  ]
-
-  const meuTemaPersonalizado = themeQuartz
-    .withParams({
-      headerTextColor: 'white',
-      headerBackgroundColor: '#BE3144',
-      headerColumnResizeHandleWidth: 0,
-    })
-    .withPart(colorSchemeDarkBlue)
-
+export function Tabela() {
   return (
-    <div className="h-80 w-[95%] md:w-[800px]">
-      <AgGridReact
-        rowData={rowData}
-        columnDefs={colDefs}
-        theme={meuTemaPersonalizado}
-      />
+    <div>
+      <h1>tabela</h1>
     </div>
   )
 }
